b1b9980e27cc3655537477f5620b499aaf3d9ef4
Manolo Battista <81776297+manolo-battista@users.noreply.github.com>
2024-06-26 14:54:04 +0200

    feat: guest components and navigation
    Co-authored-by: Manolo Battista <m.battista@codermine.com>

--- components/layout/navbar.tsx
+++ components/layout/navbar.tsx
@@ -1,8 +1,10 @@
 import Logo from "@/components/common/logo";
 
-export default function Navbar() {
+interface NavbarProps {}
+
+export default function Navbar(props: NavbarProps) {
   return (
-    <nav className="p-4 bg-background-active border-b">
+    <nav className="p-4 bg-background-active border-b border-gray">
       <div />
       <div className="flex flex-1 justify-center">
         <Logo className="w-44" />
--- components/pages/guest/fill-qi-form/fill-qi-form.tsx
+++ components/pages/guest/fill-qi-form/fill-qi-form.tsx
@@ -1,12 +1,50 @@
 import { Typography } from "@/components/ui/typography";
+import { Button } from "@/components/ui/button";
+import { ArrowLeftIcon } from "@/components/icons/arrow-left-icon";
+import { ArrowRightIcon } from "@/components/icons/arrow-right-icon";
+import StepperFooter from "@/components/pages/guest/_common/stepper-footer";
+import React from "react";
+import { useStepper } from "@/hooks/useStepper";
+import Divider from "@/components/ui/divider";
+import NeedSupport from "@/components/common/need-support";
 
 export default function FillQiForm() {
+  const { onPreviousStep, onNextStep } = useStepper();
   return (
     <div>
       <Typography variant="body-lg" className="text-center">
         In order to confirm your presence as a guest, we need you to fill and
         sign the QI form
       </Typography>
+
+      <div className="mt-8 h-[800px] bg-[#525659]" />
+
+      <StepperFooter
+        message="Have you filled an signed the QI form?"
+        action={
+          <div className="flex gap-4">
+            <Button
+              startIcon={
+                <div onClick={onPreviousStep}>
+                  <ArrowLeftIcon />
+                </div>
+              }
+              endIcon={
+                <div onClick={onNextStep}>
+                  <ArrowRightIcon />{" "}
+                </div>
+              }
+            >
+              Submit
+            </Button>
+          </div>
+        }
+      />
+
+      <Divider />
+      <div className="mt-8 flex justify-center">
+        <NeedSupport />
+      </div>
     </div>
   );
 }
