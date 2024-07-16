75dc592dc2141aec8ada5f1de2798c09710ff70f
Manolo Battista <m.battista@codermine.com>
2024-07-16 17:30:39 +0200

    feat: readonly on input

--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -24,7 +24,7 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
             type={type}
             className={cn(
               commonStyle,
-              "placeholder:text-muted-foreground flex w-full rounded-md border-input bg-background ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
+              "flex h-10 px-3 py-2 placeholder:text-muted-foreground w-full rounded-md border-input bg-background ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
             )}
             ref={ref}
             {...props}
